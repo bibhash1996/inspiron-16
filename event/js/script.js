9777ed8c7c4b6d2fc1a06e8cfd508753be72bf84
bibhash1996 <bibhash1996@gmail.com>
2016-11-10 08:28:16 +0530

    events page

---
+++ event/js/script.js
@@ -0,0 +1,254 @@
+
+function closeall()
+{
+	 document.getElementById("de1").style.width = "0%";	
+	  document.getElementById("de2").style.width = "0%";	
+	   document.getElementById("de3").style.width = "0%";	
+	    document.getElementById("de4").style.width = "0%";	
+	     document.getElementById("de5").style.width = "0%";	
+	      document.getElementById("de6").style.width = "0%";	
+	       document.getElementById("de7").style.width = "0%";	
+	        document.getElementById("de8").style.width = "0%";	
+	         document.getElementById("de9").style.width = "0%";	
+	          document.getElementById("d2e1").style.width = "0%";	
+	 			 document.getElementById("d2e2").style.width = "0%";	
+	   				document.getElementById("d2e3").style.width = "0%";	
+	    			document.getElementById("d2e4").style.width = "0%";	
+	     			document.getElementById("d2e5").style.width = "0%";	
+	      			document.getElementById("d2e6").style.width = "0%";	
+	       			document.getElementById("d2e7").style.width = "0%";	
+	        		document.getElementById("d2e8").style.width = "0%";	
+	         		document.getElementById("d2e9").style.width = "0%";	
+	        	
+	        
+
+
+}
+
+function opende1() {
+
+    document.getElementById("de1").style.width = "100%";
+    document.getElementById("de1").style.visibility ="visible";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closede1() {
+    document.getElementById("de1").style.width = "0%";
+    document.getElementById("de1").style.visibility = "hidden";
+   
+}
+
+function opende2() {
+
+    document.getElementById("de2").style.width = "100%";
+    document.getElementById("de2").style.visibility ="visible";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closede2() {
+    document.getElementById("de2").style.width = "0%";
+    document.getElementById("de2").style.visibility = "hidden";
+   
+}
+
+function opende3() {
+
+    document.getElementById("de3").style.width = "100%";
+    document.getElementById("de3").style.visibility ="visible";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closede3() {
+    document.getElementById("de3").style.width = "0%";
+    document.getElementById("de3").style.visibility = "hidden";
+   
+
+}
+function opende4() {
+
+    document.getElementById("de4").style.width = "100%";
+    document.getElementById("de4").style.visibility ="visible";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closede4() {
+    document.getElementById("de4").style.width = "0%";
+    document.getElementById("de4").style.visibility = "hidden";
+   
+}
+function opende5() {
+
+    document.getElementById("de5").style.width = "100%";
+    document.getElementById("de5").style.visibility ="visible";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closede5() {
+    document.getElementById("de5").style.width = "0%";
+    document.getElementById("de5").style.visibility = "hidden";
+   
+}
+
+function opende6() {
+
+    document.getElementById("de6").style.width = "100%";
+    document.getElementById("de6").style.visibility ="visible";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closede6() {
+    document.getElementById("de6").style.width = "0%";
+    document.getElementById("de6").style.visibility = "hidden";
+   
+}
+function opende7() {
+
+    document.getElementById("de7").style.width = "100%";
+    document.getElementById("de7").style.visibility ="visible";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closede7() {
+    document.getElementById("de7").style.width = "0%";
+    document.getElementById("de7").style.visibility = "hidden";
+   
+}
+function opende8() {
+
+    document.getElementById("de8").style.width = "100%";
+    document.getElementById("de8").style.visibility ="visible";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closede8() {
+    document.getElementById("de8").style.width = "0%";
+    document.getElementById("de8").style.visibility = "hidden";
+   
+}
+function opende9() {
+
+    document.getElementById("de9").style.width = "100%";
+    document.getElementById("de9").style.visibility ="visible";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closede9() {
+    document.getElementById("de9").style.width = "0%";
+    document.getElementById("de9").style.visibility = "hidden";
+   
+ }
+
+ function opend2e1() {
+
+    document.getElementById("d2e1").style.width = "100%";
+    document.getElementById("d2e1").style.visibility ="visible";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closed2e1() {
+    document.getElementById("d2e1").style.width = "0%";
+    document.getElementById("d2e1").style.visibility = "hidden";
+   
+}
+
+function opend2e2() {
+
+    document.getElementById("d2e2").style.width = "100%";
+    document.getElementById("d2e2").style.visibility ="visible";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closed2e2() {
+    document.getElementById("d2e2").style.width = "0%";
+    document.getElementById("d2e2").style.visibility = "hidden";
+   
+}
+
+function opend2e3() {
+
+    document.getElementById("d2e3").style.width = "100%";
+    document.getElementById("d2e3").style.visibility ="visible";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closed2e3() {
+    document.getElementById("d2e3").style.width = "0%";
+    document.getElementById("d2e3").style.visibility = "hidden";
+   
+
+}
+function opend2e4() {
+
+    document.getElementById("d2e4").style.width = "100%";
+    document.getElementById("d2e4").style.visibility ="visible";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closed2e4() {
+    document.getElementById("d2e4").style.width = "0%";
+    document.getElementById("d2e4").style.visibility = "hidden";
+   
+}
+function opend2e5() {
+
+    document.getElementById("d2e5").style.width = "100%";
+    document.getElementById("d2e5").style.visibility ="visible";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closed2e5() {
+    document.getElementById("d2e5").style.width = "0%";
+    document.getElementById("d2e5").style.visibility = "hidden";
+   
+}
+
+function opend2e6() {
+
+    document.getElementById("d2e6").style.width = "100%";
+    document.getElementById("d2e6").style.visibility ="visible";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closed2e6() {
+    document.getElementById("d2e6").style.width = "0%";
+    document.getElementById("d2e6").style.visibility = "hidden";
+   
+}
+function opend2e7() {
+
+    document.getElementById("d2e7").style.width = "100%";
+    document.getElementById("d2e7").style.visibility ="visible";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closed2e7() {
+    document.getElementById("d2e7").style.width = "0%";
+    document.getElementById("d2e7").style.visibility = "hidden";
+   
+}
+function opend2e8() {
+
+    document.getElementById("d2e8").style.width = "100%";
+    document.getElementById("d2e8").style.visibility ="visible";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closed2e8() {
+    document.getElementById("d2e8").style.width = "0%";
+    document.getElementById("d2e8").style.visibility = "hidden";
+   
+}
+function opend2e9() {
+
+    document.getElementById("d2e9").style.width = "100%";
+    document.getElementById("d2e9").style.visibility ="visible";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closed2e9() {
+    document.getElementById("d2e9").style.width = "0%";
+    document.getElementById("d2e9").style.visibility = "hidden";
+   
+ }
+
+ 
